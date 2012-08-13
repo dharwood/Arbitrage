378139508e90748bc4d25405ed3a29df3498eab6
David Harwood <dharwood@cecs.pdx.edu>
2012-08-12 21:04:32 -0700

    the refactoring work continues

--- arbitrage.py
+++ arbitrage.py
@@ -7,8 +7,9 @@
 #copy of the license at http://opensource.org/licenses/MIT
 #####
 
-import sys, time, select, collections, socket, asyncore, asynchat
+import sys, time, select, collections, socket, asyncore, asynchat, random, threading
 
+#=====Area class=====
 class Area:
     
     def __init__(self, nodes, objects, connections, player):
@@ -18,14 +19,14 @@ class Area:
         self.players = [player]
 
     def buildactions(self, turns):
-        out = []
+        out = list()
         for x in nodes:
             out.append(("loc" + str(x.idnum), "Enter node " + x.name))
         for y in connections:
-            out.append(("loc" + str(y), "Go to area " + str(y))
-        #if there's anything else that we want to pull actions from, this is where the code for that would go
+            out.append(("loc" + str(y), "Go to area " + str(y)))
         return out
 
+#=====Node class=====
 #nodes need to have a name, a type, the various resource prices, a size (however that's measured for the skin in question), and a point at which it was last accessed
 #every time the node is queried, we can compare the last access point with the current time and (if the difference is greater than some amount of time) we can then use some formula to determine what the new size and resource prices should be, as well as apply the most recent time to the string to make sure that we're not reading the same node info a bunch of time
 #this is kind of simple, but we can modify it later (nodes that produce one kind of resource could engage in small amounts of local trade with nodes in the same area, for example, and that could influence prices)
@@ -39,106 +40,21 @@ class Node:
         self.sellrates = sellrates
         self.size = size
 
-    def buying(self, resource, quantity):
+    def buying(self, resource, quantity): pass
         #this is for the player buying from the node
 
-nodeinfofile = open("nodeinfo") #this is just a placeholder for now, will be chnaged when the configuration stuff is done
+#nodeinfofile = open("nodeinfo") #this is just a placeholder for now, will be chnaged when the configuration stuff is done
 
 #this would be where the thing to generate a node belongs, depending on parameters in the nodeinfo file
 #...though, come to think of it, this might be better served by being in the world building section, and leave in-game node creation to a different part of the code
 #that way, I only have to read in the file once, and it's one less thing to worry about having to import or deal with from another file
 
-#def __init__(self):
-#    self.activeusers = list() #this might change, depending on how (if?) I decide to store nodes in memory
-#    self.activeareas = dict() #areaID -> area def
-#    self.activenodes = dict() #objectID -> object def
-#    self.activeloc = dict() #locationID -> location def
-
-def addplayer(self, player):#this whole function needs some more work since I'm not using sql to store player info anymore but am instead using memory, can store player info in a dict when they're not active (and, for that matter, a dict when they are), now that I think about it, do I really need to have an activeplayers list? it would probably still be good to have something set up to keep track of who is logged in and who isn't to keep users from trying to manipulate other people's accounts
-    self.activeusers.append(player)
-#    locID = player.getloc()
-#    if locID not in self.activeloc():
-#        if locID > 1000000:
-#            self.activeloc[locID] = #sql for nodes needs to go here
-#        else:
-#            self.activeloc[locID] = #sql for areas needs to go here
-#    self.buildactions(player) #this will now work by storing the available actions in the player's "file" and just reading them as usual from there when a player logs in
-
-def removeplayer(self, player): #it feels like there should be more to this, but if everything is starting out instantiated
-    self.activeusers.remove(player)
-
-def buildactions(self, player):
-    location = self.activeloc[player.loc]
-    #need to go through every object, node, and warp in the area (or just allow for purchasing or leaving if at a node) in order to make this work. there's also the non-loaction specific stuff (send messages, open menu, etc), but I think most of that goes into a different function(s)
-    actions = location.buildactions(player.turns) #this should be a list of tuples with format ("identifier", "description")
-    actions.append(player.buildactions()) #I know I'm breaking the standard of making pulling info as "getfoo" but I want to use getactions to retrieve the list of actions that's already built, not build the new list of actions
-    player.setactions(actions)
-    #pass #so here, I need to go through the list of stuff in the player's location and determine what all of the possible actions are, then give (or return?) that list to the player
-
-def performactions(self, player, action):
-    if action.startswith('loc'): #when changing location
-        self.changeloc(player, int(action[3:]))
-    elif action.startswith('buy'):
-        self.resourceexchange(player, int(action[3:4]), int(action[4:]), True)
-    elif action.startswith('sel'):
-        self.resourceexcahnge(player, int(action[3:4]), int(action[4:]), False)
-    else:
-        actionlists.perform(player.getaction(action), player) #this line currently does nothing (all of the core actions are here in the engine, and there's nothing else implemented yet)
-    #OK, so, given that the action lists aren't really supposed to have access the specific player performing the action, is there a way I can pull out the action that I want to have happen rather than trying to call that code in place? is there something with virtual functions or lambda calculus I can use?
-    pass #gonna have to work more on this...
-
-#I'm putting some actions that are tied to nodes and areas (moving, buying, selling) here into the engine. As object and other features are implemented, actions for them can be placed in other areas
-def changelocation(self, player, movingto):
-#    if not self.turnuse(): #if the player doesn't have enough turns left
-#        return 'Action not completed: insufficient turns.'
-    locID = player.loc
-    player.loc = movingto
-    self.activeloc[locID].remove(player)
-#    if len(self.activeloc[locID].players) is 0:
-#        if locID > 1000000:
-#            #need to write node state to node table
-#        else:
-#            #need to write area state to area table
-#        del self.activeloc[locID]
-#    if movingto not in self.activeloc:
-#        if locID > 1000000:
-#            self.activeloc[movingto] = #sql for nodes needs to go here
-#        else:
-#            self.activeloc[movingto] = #sql for areas needs to go here
-    self.activeloc[locID].append(player)
-
-def resourceexchange(self, player, resource, number, buying):
-    #hmm, I wonder how python will like just going "location.changeprice" That seems like it should fail, but it might not...
-    if buying: #buying resources from the node
-        if number > player.freeholds:
-            return "Insufficient Space"
-        if player.money < (self.activeloc[player.loc].buyprice[resource] * number):
-            return "Insufficient Funds"
-        player.resourcelist[resource] += number
-        player.freeholds -= number
-        player.money -= (self.activeloc[player.loc].buyprice[resource] * number)
-        self.activeloc[player.loc].pricechange(resource, number)
-    else: #selling resources to the node
-        if number > player.resourcelist[resource]:
-            return "Insufficient Supply"
-        player.resourcelist[resource] -= number
-        player.freeholds += number
-        player.money += (self.activeloc[player.loc].sellprice[resource] * number)
-        self.activeloc[player.loc.pricechange(resource * -1, number)
-
-#def turnuse(self, player, turns=1):
-#    if player.turns < turns:
-#        player.turns -= turns
-#        return True
-#    else:
-#        return False
-
-
-class server(asyncore.dispatcher):
+#=====Server class=====
+class Server(asyncore.dispatcher):
 
     def __init__(self, host, port):
         asynchat.async_chat.__init__(self)
-        self.inputbuf = []
+        self.inputbuf = list()
         self.set_terminator(b"\r\n\r\n")
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.set_reuse_addr()
@@ -162,9 +78,8 @@ class server(asyncore.dispatcher):
         pass #I'm really just kinda creating an interface here, aren't I?
 
     #here's something to think about: the session objects for players, why don't I just make them extended async_chat objects? that way, I can hold all of the player info and trust them to take care of themselves when the player sends an action/connects/quits/whatever. There's probably some reason this is going to fail horibly, but I'll worry about that when I get there since nothing seems to come to mind right now about how things could go wrong...
-    #I mean, I'll still need to store a list of active players and such to keep garbage collecting from getting it (wait, is there a built in garbage collector for python? I should double check that. If not, I could just leave them floating around out there and trust the loop to keep looking after them until the user disconnects), as well as the active areas and nodes (those I definitely need to get done)
 
-
+#=====Session class=====
 class Session:
     
     def __init__(self, playerID, playerName, location, turns, vehicle, money):
@@ -177,10 +92,80 @@ class Session:
         self.resourcelist = [0,0,0] #this might take some rethinking...
         self.freeholds = 50 #this is something that will be changable with upgrades (eventually)
 
+    state = "" #this holds the last message that was sent to the player
+    message = "" #small response to the player doing something that doesn't need everything redone ("Insufficient funds", for example)
 
-if __name__ == "__main__":
-    for i in range(50): #this seems like a good number to start with
+#=====functions=====
+#def __init__(self):
+#    self.activeusers = list() #this might change, depending on how (if?) I decide to store nodes in memory
+#    self.activeareas = dict() #areaID -> area def
+#    self.activenodes = dict() #objectID -> object def
+#    self.activeloc = dict() #locationID -> location def
+
+#def addplayer(users, player):#this whole function needs some more work since I'm not using sql to store player info anymore but am instead using memory, can store player info in a dict when they're not active (and, for that matter, a dict when they are), now that I think about it, do I really need to have an activeplayers list? it would probably still be good to have something set up to keep track of who is logged in and who isn't to keep users from trying to manipulate other people's accounts
+#    users.append(player)
+#    return users
+
+#def removeplayer(users, player): #it feels like there should be more to this, but if everything is starting out instantiated, then maybe not
+#    users.remove(player)
+#    return users
+
+def buildactions(player):
+    location = activeloc[player.loc]
+    #need to go through every object, node, and connection in the area (or just allow for purchasing or leaving if at a node) in order to make this work. there's also the non-loaction specific stuff (send messages, open menu, etc), but I think most of that goes into a different function(s)
+    actions = location.buildactions(player.turns) #this should be a list of tuples with format ("identifier", "description")
+    actions.append(player.buildactions()) #I know I'm breaking the standard of making pulling info as "getfoo" but I want to use getactions to retrieve the list of actions that's already built, not build the new list of actions
+    player.setactions(actions)
+    #pass #so here, I need to go through the list of stuff in the player's location and determine what all of the possible actions are, then give (or return?) that list to the player
+
+def performactions(player, action):
+    if action.startswith('loc'): #when changing location
+        changeloc(player, int(action[3:]))
+    elif action.startswith('buy'):
+        resourceexchange(player, int(action[3:4]), int(action[4:]), True)
+    elif action.startswith('sel'):
+        resourceexcahnge(player, int(action[3:4]), int(action[4:]), False)
+    else:
+        actionlists.perform(player.getaction(action), player) #this line currently does nothing (all of the core actions are here in the engine, and there's nothing else implemented yet)
+    #OK, so, given that the action lists aren't really supposed to have access the specific player performing the action, is there a way I can pull out the action that I want to have happen rather than trying to call that code in place? is there something with virtual functions or lambda calculus I can use?
+    pass #gonna have to work more on this...
+
+#I'm putting some actions that are tied to nodes and areas (moving, buying, selling) here into the engine. As object and other features are implemented, actions for them can be placed in other areas
+def changelocation(player, movingto):
+    locID = player.loc
+    player.loc = movingto
+    activeloc[locID].remove(player)
+    activeloc[locID].append(player)
+
+def resourceexchange(player, resource, number, buying):
+    #hmm, I wonder how python will like just going "location.changeprice" That seems like it should fail, but it might not...
+    if buying: #buying resources from the node
+        if number > player.freeholds:
+            return "Insufficient Space"
+        if player.money < (activeloc[player.loc].buyprice[resource] * number):
+            return "Insufficient Funds"
+        player.resourcelist[resource] += number
+        player.freeholds -= number
+        player.money -= (activeloc[player.loc].buyprice[resource] * number)
+        activeloc[player.loc].pricechange(resource, number)
+    else: #selling resources to the node
+        if number > player.resourcelist[resource]:
+            return "Insufficient Supply"
+        player.resourcelist[resource] -= number
+        player.freeholds += number
+        player.money += (activeloc[player.loc].sellprice[resource] * number)
+        activeloc[player.loc.pricechange(resource * -1, number)]
+
+def priceupdate():
+    for i in areas:
+        for j in i.nodes:
+            pass #this is where the price updating logic goes
+    t = Timer(3600.0, priceupdate)
+    t.start()
+
+if __name__ == '__main__': pass
+#    for i in range(50): #this seems like a good number to start with
         #steps here: generate area, generate 0-3 (-ish) nodes in that area, then build in...oh let's say 5-10 connections to other areas (should I worry about areas being unreachable? could go through and do a count after the first run)
-        a = area.area()
-        for j in random.randrange(3): #this is where the 0-3 nodes are created and placed in the area
-            n = node.node(random.randint(0,3),"Node " + str(i) + "-" + str(j), )
+ #       a = area.area()
+ #       for j in random.randrange(3): #this is where the 0-3 nodes are created and placed in the area
+ #           n = node.node(random.randint(0,3),"Node " + str(i) + "-" + str(j), )
